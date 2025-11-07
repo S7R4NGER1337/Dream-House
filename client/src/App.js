2a6db9b0e831ffcf710570d1085411f5e1d60e27
S7R4NGER3301 <nikolakoew@gmail.com>
2025-11-07 17:03:01 +0200

    creating Home component and HeroSection component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from './components/Nav';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
      <Nav />
+     <Home />
     </div>
   );
 }
---
+++ client/src/pages/home/HeroSection.js
@@ -0,0 +1,5 @@
+import styles from './heroSection.module.css'
+
+export default function HeroSection() {
+    return <h1>Test</h1>
+}
