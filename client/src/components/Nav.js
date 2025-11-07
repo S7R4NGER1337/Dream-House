af43f5a2d2138371a47b4e0394d1624a43655e31
S7R4NGER3301 <nikolakoew@gmail.com>
2025-11-07 16:46:46 +0200

    setting class name selected by selected pathname

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,17 +1,53 @@
 import styles from "./nav.module.css";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 export default function Nav() {
+  const location = useLocation().pathname;
+
   return (
     <div className={styles.navContainer}>
       <section className={styles.siteLogoContainer}>
         <p className={styles.siteName}>Dream House</p>
       </section>
       <section className={styles.siteLinksContainer}>
-        <Link className={styles.siteLink} to="/properties">Properties</Link>
-        <Link className={styles.siteLink} to="/services">Services</Link>
-        <Link className={styles.siteLink} to="/aboutUs">AboutUs</Link>
-        <Link className={styles.siteLink} to="/contact">Contact</Link>
+        <Link
+          className={location === "/" ? styles.selected : styles.siteLink}
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          className={
+            location === "/properties" ? styles.selected : styles.siteLink
+          }
+          to="/properties"
+        >
+          Properties
+        </Link>
+        <Link
+          className={
+            location === "/services" ? styles.selected : styles.siteLink
+          }
+          to="/services"
+        >
+          Services
+        </Link>
+        <Link
+          className={
+            location === "/aboutUs" ? styles.selected : styles.siteLink
+          }
+          to="/aboutUs"
+        >
+          AboutUs
+        </Link>
+        <Link
+          className={
+            location === "/contact" ? styles.selected : styles.siteLink
+          }
+          to="/contact"
+        >
+          Contact
+        </Link>
       </section>
     </div>
   );
