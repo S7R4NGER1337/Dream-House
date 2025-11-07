bcb163cae0e8968c4cce8e331a2541497eeb3c27
S7R4NGER3301 <nikolakoew@gmail.com>
2025-11-07 18:32:33 +0200

    rendering handpicked for you in home

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,9 +1,11 @@
+import HandpickedForYou from "./HandpickedForYou";
 import HeroSection from "./HeroSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <HandpickedForYou />
     </>
   );
 }
