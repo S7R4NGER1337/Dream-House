8c169b956be8950fb351136c8d5878bb9bda2bda
S7R4NGER3301 <nikolakoew@gmail.com>
2025-11-07 17:20:06 +0200

    adding elements to hero section

--- client/src/pages/home/HeroSection.js
+++ client/src/pages/home/HeroSection.js
@@ -1,5 +1,30 @@
-import styles from './heroSection.module.css'
+import styles from "./heroSection.module.css";
 
 export default function HeroSection() {
-    return <h1>Test</h1>
+  return (
+    <div className={styles.heroSectionContainer}>
+      <h1 className={styles.heroSectionTitle}>Your Journey Home Starts Here</h1>
+      <p className={styles.heroSectionSubtitle}>
+        Discover a curated selection of premier properties and connect with
+        expert agents who are dedicated to finding your perfect home.
+      </p>
+      <div className={styles.heroSectionFilters}>
+        <input className={styles.heroSectionFilterInput} type="text" placeholder="Enter a location or keyword" />
+        <select name="type"  className={styles.heroSectionFilterInput} >
+          <option value="">Property Type</option>
+          <option value="volvo">House</option>
+          <option value="saab">Apartment</option>
+          <option value="mercedes">Condo</option>
+        </select>
+        <select name="price"  className={styles.heroSectionFilterInput} >
+          <option value="">Price Range</option>
+          <option value="volvo">$200k - $400k</option>
+          <option value="volvo">$400k - $600k</option>
+          <option value="volvo">$600k - $800k</option>
+          <option value="volvo">$800k+</option>
+        </select>
+        <button className={styles.heroSectionButton} >Search</button>
+      </div>
+    </div>
+  );
 }
