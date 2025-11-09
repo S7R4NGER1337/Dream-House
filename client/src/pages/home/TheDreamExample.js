690abc76225660452584da31ef91815a00ec6acb
S7R4NGER3301 <nikolakoew@gmail.com>
2025-11-09 17:10:45 +0200

    adding jsx to TheDreamExample component

--- client/src/pages/home/TheDreamExample.js
+++ client/src/pages/home/TheDreamExample.js
@@ -1,5 +1,15 @@
-import styles from './theDreamExample.module.css'
+import styles from "./theDreamExample.module.css";
 
-export default function TheDreamExample(params) {
-    
+export default function TheDreamExample({ dreamData }) {
+  return (
+    <div className={styles.theDreamExampleContainer}>
+      <img
+        className={styles.theDreamExampleIcon}
+        src={dreamData.iconPath}
+        alt="dreamIcon"
+      />
+      <h1 className={styles.theDreamExampleName}>{dreamData.name}</h1>
+      <p className={styles.theDreamExampleText}>{dreamData.text}</p>
+    </div>
+  );
 }
