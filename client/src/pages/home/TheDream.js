494c140b6eb9c47566136d811a5a6b3eebec3261
S7R4NGER3301 <nikolakoew@gmail.com>
2025-11-09 16:54:14 +0200

    creating TheDream and TheDreamExample components

---
+++ client/src/pages/home/TheDream.js
@@ -0,0 +1,5 @@
+import styles from './theDream.module.css'
+
+export default function TheDream(params) {
+    
+}
---
+++ client/src/pages/home/TheDreamExample.js
@@ -0,0 +1,5 @@
+import styles from './theDreamExample.module.css'
+
+export default function TheDreamExample(params) {
+    
+}
