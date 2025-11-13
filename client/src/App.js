0cbb5ffacb41f3e254308ab1ba0aa1eb22653d40
S7R4NGER3301 <nikolakoew@gmail.com>
2025-11-13 02:11:19 +0200

    creating routes for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Home from "./pages/home/Home";
+import AboutUs from "./pages/aboutUs/AboutUs";
 
 function App() {
   return (
     <>
       <Nav />
       <div style={{marginTop: '5rem'}}>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutUs" element={<AboutUs />} />
+        </Routes>
       </div>
       <Footer />
     </>
