6e2b754b13cf9a00c4709cd0b361b04abf3bfb67
kali <nikolakoew@gmail.com>
2025-12-22 15:12:03 +0200

    Adding some basic info about the house and styling it.

--- client/src/pages/property/Property.js
+++ client/src/pages/property/Property.js
@@ -1,3 +1,16 @@
-export default function Property(params) {
-    return <h1>Test</h1>
+import styles from './property.module.css'
+
+export default function Property() {
+    return <div className={styles.propertyContainer}>
+        <div className={styles.nameAndPriceContainer}>
+            <section className={styles.nameAndStreet}>
+                <h1 className={styles.houseName}>Modern Suburban Oasis</h1>
+                <p className={styles.houseStreet}>123 Maple Street, Sunnyvale, CA 94087</p>
+            </section>
+            <section className={styles.housePriceContainer}>
+                <h1 className={styles.housePrice}>$1,200,000</h1>
+                <p className={styles.houseMortgage}>Est. Mortgage: $6,500/mo</p>
+            </section>
+        </div>
+    </div>
 }
