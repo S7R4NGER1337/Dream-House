70019e7f028b68c0c3bcc25e2db11ca0cc54fe0b
kali <nikolakoew@gmail.com>
2025-12-22 18:09:28 +0200

    Adding photos gallery

--- client/src/pages/property/Property.js
+++ client/src/pages/property/Property.js
@@ -1,16 +1,39 @@
-import styles from './property.module.css'
+import styles from './property.module.css';
 
 export default function Property() {
-    return <div className={styles.propertyContainer}>
-        <div className={styles.nameAndPriceContainer}>
-            <section className={styles.nameAndStreet}>
-                <h1 className={styles.houseName}>Modern Suburban Oasis</h1>
-                <p className={styles.houseStreet}>123 Maple Street, Sunnyvale, CA 94087</p>
-            </section>
-            <section className={styles.housePriceContainer}>
-                <h1 className={styles.housePrice}>$1,200,000</h1>
-                <p className={styles.houseMortgage}>Est. Mortgage: $6,500/mo</p>
-            </section>
+    return (
+        <div className={styles.propertyContainer}>
+            <div className={styles.nameAndPriceContainer}>
+                <section className={styles.nameAndStreet}>
+                    <h1 className={styles.houseName}>Modern Suburban Oasis</h1>
+                    <p className={styles.houseStreet}>123 Maple Street, Sunnyvale, CA 94087</p>
+                </section>
+                <section className={styles.housePriceContainer}>
+                    <h1 className={styles.housePrice}>$1,200,000</h1>
+                    <p className={styles.houseMortgage}>Est. Mortgage: $6,500/mo</p>
+                </section>
+            </div>
+
+            <div className={styles.photosGrid}>
+                <div className={`${styles.photoWrapper} ${styles.mainPhoto}`}>
+                    <img src='/unnamed.png' alt='Main view' />
+                </div>
+                <div className={styles.photoWrapper}>
+                    <img src='/unnamed (2).png' alt='Interior' />
+                </div>
+                <div className={styles.photoWrapper}>
+                    <img src='/unnamed (3).png' alt='Pool' />
+                </div>
+                <div className={styles.photoWrapper}>
+                    <img src='/unnamed.png' alt='Backyard' />
+                </div>
+                <div className={`${styles.photoWrapper} ${styles.lastPhoto}`}>
+                    <img src='/unnamed (1).png' alt='Night view' />
+                    <button className={styles.showAllBtn}>
+                        <span className={styles.icon}>⠿</span> Show all photos
+                    </button>
+                </div>
+            </div>
         </div>
-    </div>
+    );
 }
